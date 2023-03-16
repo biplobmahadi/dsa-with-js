4911bb5c71b3614e31a552ad7624f80dc69b22b9
Biplob <biplob.ruet15@gmail.com>
2023-03-16 09:51:51 +0600

    create linked list with append, prepend, insert functionality

---
+++ linkedList/myLinkList.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value)
+    this.tail = this.head
+    this.len = 1
+  }
+
+  append(value) {
+    const newNode = new Node(value)
+    this.tail.next = newNode
+    this.tail = newNode
+    this.len++
+  }
+
+  prepend(value) {
+    const newNode = new Node(value)
+    newNode.next = this.head
+    this.len++
+    this.head = newNode
+  }
+
+  printList() {
+    const arr = []
+    let currentNode = this.head
+    while (currentNode !== null) {
+      arr.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return arr
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value)
+    } else {
+      let i = 1
+      let currentNode = this.head
+      while (currentNode !== null) {
+        if (i === index) {
+          const newNode = new Node(value)
+          newNode.next = currentNode.next
+          currentNode.next = newNode
+          break
+        }
+        currentNode = currentNode.next
+        i++
+      }
+    }
+  }
+}
+
+const myLinkedList = new LinkedList(10)
+myLinkedList.append(5)
+myLinkedList.append(6)
+myLinkedList.prepend(60)
+myLinkedList.insert(2, 500)
+console.log(myLinkedList.printList())
