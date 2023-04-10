f8291b7c6ccfb6bbff1a8c2ff3479d6f36d3f34c
Biplob <biplob.ruet15@gmail.com>
2023-04-10 17:36:32 +0600

    add dynamic programming with recursion

--- algo/recursion/fibonacci.js
+++ algo/recursion/fibonacci.js
@@ -1,3 +1,6 @@
+let i = 0
+let j = 0
+
 const fiboIteration = (n) => {
   // check extra
   let res
@@ -14,10 +17,38 @@ const fiboIteration = (n) => {
 }
 
 const fiboRecursion = (n) => {
+  i++
   // check extra
   if (n < 2) return n
   return fiboRecursion(n - 1) + fiboRecursion(n - 2)
 }
 
+const memoFib = () => {
+  const cache = {}
+  return function fib(n) {
+    j++
+    if (!cache[n]) {
+      if (n < 2) return n
+      cache[n] = fib(n - 1) + fib(n - 2)
+    }
+
+    return cache[n]
+  }
+}
+
+const fib = memoFib()
+
+const fiboDPBottomUp = (n) => {
+  const arr = [0, 1]
+
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2])
+  }
+  return arr.pop()
+}
+
 console.log(fiboIteration(40)) // O(n)
+console.log(fib(40)) // O(n) & O(n)
+console.log(fiboDPBottomUp(40)) // O(n)
 console.log(fiboRecursion(40)) // O(2^n)
+console.log('rec', i, 'dp', j)
