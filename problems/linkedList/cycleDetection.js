46471f7e3f7c87cb38cfd0a0d828e7a5a0a2f74d
biplob <biplobhasan547@gmail.com>
2023-04-28 18:21:50 +0600

    find the starting node of cycle with optimal soln

--- problems/linkedList/cycleDetection.js
+++ problems/linkedList/cycleDetection.js
@@ -23,3 +23,30 @@ const cycleStartNode = (head) => {
 
   return null
 }
+
+const cycleStartNodeOptimal = (head) => {
+  if (head === null) return null
+
+  let tortoise = head
+  let hare = head
+  let count = 0
+
+  while ((!(tortoise === hare) && hare) || count < 1) {
+    count++
+    tortoise = tortoise.next
+
+    if (hare.next) hare = hare.next.next
+    else hare = null
+  }
+
+  if (hare) {
+    let startNode = head
+    while (!(startNode === hare)) {
+      hare = hare.next
+      startNode = startNode.next
+    }
+    return startNode
+  }
+
+  return null
+}
