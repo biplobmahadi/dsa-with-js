926f8e72ae5370d93853264f788190ae27e311b4
biplob <biplobhasan547@gmail.com>
2023-04-28 18:21:10 +0600

    find the starting node of cycle in LL

--- problems/linkedList/cycleDetection.js
+++ problems/linkedList/cycleDetection.js
@@ -10,3 +10,16 @@ const isCycleInLL = (head) => {
 
   return false
 }
+
+const cycleStartNode = (head) => {
+  const set = new Set()
+  let currentNode = head
+
+  while (currentNode) {
+    if (set.has(currentNode)) return currentNode
+    set.add(currentNode)
+    currentNode = currentNode.next
+  }
+
+  return null
+}
