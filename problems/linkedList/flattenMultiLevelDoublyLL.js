b22f1410da0dc11a24fa99b527d35489eb3ee261
biplob <biplobhasan547@gmail.com>
2023-04-28 12:18:10 +0600

    solve flatten multi level doubly linked list problem

--- problems/linkedList/flattenMultiLevelDoublyLL.js
+++ problems/linkedList/flattenMultiLevelDoublyLL.js
@@ -1,2 +1,24 @@
 // Go top to bottom by merging
-const flattenMultiLevelDoublyLL = (head) => {}
+const flattenMultiLevelDoublyLL = (head) => {
+  let currentNode = head
+
+  while (currentNode) {
+    if (currentNode.child) {
+      let newCurrentNode = currentNode.child
+      while (newCurrentNode.next) {
+        newCurrentNode = newCurrentNode.next
+      }
+      if (currentNode.next) {
+        currentNode.next.prev = newCurrentNode
+      }
+      newCurrentNode.next = currentNode.next
+      currentNode.next = currentNode.child
+      currentNode.child.prev = currentNode
+      currentNode.child = null
+    }
+
+    currentNode = currentNode.next
+  }
+
+  return head
+}
