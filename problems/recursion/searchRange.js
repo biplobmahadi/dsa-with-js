9f9801e81f3a41bcad2757b1c15f6140d59c1261
biplob <biplobhasan547@gmail.com>
2023-05-06 00:48:00 +0600

    add optimal soln for search range in sorted array using binary search algo

--- problems/recursion/searchRange.js
+++ problems/recursion/searchRange.js
@@ -20,4 +20,41 @@ const searchRange = (arr, t) => {
   }
 }
 
-const searchRangeOptimal = (arr, t) => {}
+const binarySearch = (arr, left, right, target) => {
+  while (right >= left) {
+    const mid = Math.floor((left + right) / 2)
+    if (arr[mid] === target) return mid
+    else if (arr[mid] > target) right = mid - 1
+    else left = mid + 1
+  }
+
+  return -1
+}
+
+const searchRangeOptimal = (arr, target) => {
+  if (arr.length === 0) return [-1, -1]
+  const targetIndex = binarySearch(arr, 0, arr.length - 1, target)
+  if (targetIndex === -1) return [-1, -1]
+
+  let start = targetIndex,
+    end = targetIndex
+  let tmpStart, tmpEnd
+
+  while (start !== -1) {
+    tmpStart = start
+    start = binarySearch(arr, 0, start - 1, target)
+  }
+  start = tmpStart
+
+  while (end !== -1) {
+    tmpEnd = end
+    end = binarySearch(arr, end + 1, arr.length - 1, target)
+  }
+  end = tmpEnd
+
+  return [start, end]
+}
+
+const arr = [1, 2, 3, 4, 5, 5, 6, 7]
+const arr2 = [5, 5, 5, 5, 5, 5, 5, 5]
+console.log(searchRangeOptimal(arr, 5))
