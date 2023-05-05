fbbc67330687a41071294f8e3325542752f9be5a
biplob <biplobhasan547@gmail.com>
2023-05-05 09:50:20 +0600

    find kth largest with optimal sol using hoare's quick select algo

--- problems/recursion/kthLargestElInArray.js
+++ problems/recursion/kthLargestElInArray.js
@@ -34,6 +34,27 @@ const swap = (arr, i, j) => {
   arr[j] = tmp
 }
 
-const arr = [23, 4, 5, 1, 6, 8, 0, 1, 9, 2, 0, 24]
-console.log(findKthLargest(arr, 2))
-console.log(arr)
+// Using Hoare's Quick Select Algo
+const findKthLargestOptimal = (arr, k) => {
+  const index = arr.length - k
+  return quickSelect(arr, 0, arr.length - 1, index)
+}
+
+const quickSelect = (arr, left, right, kthSmallest) => {
+  if (left < right) {
+    const pivot = getPivot(arr, left, right)
+    if (pivot === kthSmallest) {
+      return arr[kthSmallest]
+    } else if (pivot < kthSmallest) {
+      return quickSelect(arr, pivot + 1, right, kthSmallest)
+    } else {
+      return quickSelect(arr, left, pivot - 1, kthSmallest)
+    }
+  }
+}
+
+const arr = [7, 6, 5, 4, 3, 2, 1]
+// console.log(findKthLargest(arr, 2)) // O(n logn) & O(logn)
+console.log(findKthLargestOptimal(arr, 5)) // O(n) & O(1) -> TCO
+
+/* For both in worst case time is O(n^2), when arr is given in descending order */
