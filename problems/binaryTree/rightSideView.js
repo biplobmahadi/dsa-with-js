95e4e3a70fc471e495900db00ea6a1e2b4c6bfe4
biplob <biplobhasan547@gmail.com>
2023-05-12 08:19:08 +0600

    practice dfs with chaning order for in, pre, post as right node priority

--- problems/binaryTree/rightSideView.js
+++ problems/binaryTree/rightSideView.js
@@ -22,3 +22,65 @@ const rightSideView = (root) => {
 
   return res
 }
+
+const root = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 3,
+      left: null,
+      right: {
+        val: 7,
+        left: {
+          val: 8,
+          left: null,
+          right: null,
+        },
+        right: null,
+      },
+    },
+    right: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 5,
+    left: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
+}
+
+const dfsPreOrder = (root, list) => {
+  if (root === null) return
+  list.push(root.val)
+  if (root.right) dfsPreOrder(root.right, list)
+  if (root.left) dfsPreOrder(root.left, list)
+  return list
+}
+const dfsInOrder = (root, list) => {
+  if (root === null) return
+  if (root.right) dfsInOrder(root.right, list)
+  list.push(root.val)
+  if (root.left) dfsInOrder(root.left, list)
+  return list
+}
+const dfsPostOrder = (root, list) => {
+  if (root === null) return
+  if (root.right) dfsPostOrder(root.right, list)
+  if (root.left) dfsPostOrder(root.left, list)
+  list.push(root.val)
+  return list
+}
+
+const rightSideViewDFS = (root) => {}
+
+console.log(dfsPreOrder(root, []))
+console.log(dfsInOrder(root, []))
+console.log(dfsPostOrder(root, []))
