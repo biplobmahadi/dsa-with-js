7c29c34f0c59d3735a487105f941cbd97eaefce2
biplob <biplobhasan547@gmail.com>
2023-05-17 22:47:03 +0600

    implement bfs for graph

---
+++ graph/bfs.js
@@ -0,0 +1,22 @@
+const bfsInGraphForAdjacencyList = (graph) => {
+  if (!graph.length) return []
+  const queue = [0]
+  const list = []
+  const seen = {}
+
+  while (queue.length) {
+    const popped = queue.shift()
+    list.push(popped)
+    seen[popped] = true
+
+    const connections = graph[popped]
+    for (let i = 0; i < connections.length; i++) {
+      if (!seen[connections[i]]) queue.push(connections[i])
+    }
+  }
+  return list
+}
+
+const graph = [[1, 3], [0], [3, 8], [0, 2, 4, 5], [3, 6], [3], [4, 7], [6], [2]]
+
+console.log(bfsInGraphForAdjacencyList(graph))
