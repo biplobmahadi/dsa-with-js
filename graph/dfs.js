700cbeff09735c39fdfa0b8611c1d4967ff6b7cb
biplob <biplobhasan547@gmail.com>
2023-05-17 23:23:50 +0600

    implement dfs for graph using adjacency list

---
+++ graph/dfs.js
@@ -0,0 +1,28 @@
+const graph = [[1, 3], [0], [3, 8], [0, 2, 4, 5], [3, 6], [3], [4, 7], [6], [2]]
+const graphInMatrix = [
+  [0, 1, 0, 1, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 1, 0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 1, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0],
+]
+
+const dfsInGraphUsingAdjacencyList = (graph, vertex, list, seen) => {
+  if (seen[vertex]) return
+
+  list.push(vertex)
+  seen[vertex] = true
+
+  const connections = graph[vertex]
+  for (let i = 0; i < connections.length; i++) {
+    dfsInGraphUsingAdjacencyList(graph, connections[i], list, seen)
+  }
+
+  return list
+}
+
+console.log(dfsInGraphUsingAdjacencyList(graph, 0, [], {}))
