aecc546006755811e8d28f06fd5928ebd5b40e7b
biplob <biplobhasan547@gmail.com>
2023-05-17 23:27:49 +0600

    implement dfs for graph using adjacency matrix

--- graph/dfs.js
+++ graph/dfs.js
@@ -26,3 +26,21 @@ const dfsInGraphUsingAdjacencyList = (graph, vertex, list, seen) => {
 }
 
 console.log(dfsInGraphUsingAdjacencyList(graph, 0, [], {}))
+
+const dfsInGraphUsingAdjacencyMatrix = (graph, vertex, list, seen) => {
+  if (seen[vertex]) return
+
+  list.push(vertex)
+  seen[vertex] = true
+
+  const connections = graph[vertex]
+  for (let i = 0; i < connections.length; i++) {
+    if (connections[i]) {
+      dfsInGraphUsingAdjacencyMatrix(graph, i, list, seen)
+    }
+  }
+
+  return list
+}
+
+console.log(dfsInGraphUsingAdjacencyMatrix(graphInMatrix, 0, [], {}))
