a826ee42fbc086a5340433dbba0821582d029bfa
biplob <biplobhasan547@gmail.com>
2023-05-26 23:25:05 +0600

    implement dijkstras algo with binary priority queue to solve network delay time problem

--- problems/graph/networkDelayTime.js
+++ problems/graph/networkDelayTime.js
@@ -1,5 +1,126 @@
+class MyPriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this._heap = []
+    this._comparator = comparator
+  }
+
+  size() {
+    return this._heap.length
+  }
+
+  peek() {
+    return this._heap[0]
+  }
+
+  isEmpty() {
+    return this._heap.length === 0
+  }
+
+  _parent(idx) {
+    return Math.floor((idx - 1) / 2)
+  }
+
+  _leftChild(idx) {
+    return idx * 2 + 1
+  }
+
+  _rightChild(idx) {
+    return idx * 2 + 2
+  }
+
+  _swap(i, j) {
+    ;[this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]]
+  }
+
+  _compare(i, j) {
+    return this._comparator(this._heap[i], this._heap[j])
+  }
+
+  push(value) {
+    this._heap.push(value)
+    this._siftUp()
+
+    return this.size()
+  }
+
+  _siftUp() {
+    let nodeIdx = this.size() - 1
+
+    while (0 < nodeIdx && this._compare(nodeIdx, this._parent(nodeIdx))) {
+      this._swap(nodeIdx, this._parent(nodeIdx))
+      nodeIdx = this._parent(nodeIdx)
+    }
+  }
+
+  pop() {
+    if (this.size() > 1) {
+      this._swap(0, this.size() - 1)
+    }
+
+    const poppedValue = this._heap.pop()
+    this._siftDown()
+    return poppedValue
+  }
+
+  _siftDown() {
+    let nodeIdx = 0
+
+    while (
+      (this._leftChild(nodeIdx) < this.size() &&
+        this._compare(this._leftChild(nodeIdx), nodeIdx)) ||
+      (this._rightChild(nodeIdx) < this.size() &&
+        this._compare(this._rightChild(nodeIdx), nodeIdx))
+    ) {
+      const greaterChildIdx =
+        this._rightChild(nodeIdx) < this.size() &&
+        this._compare(this._rightChild(nodeIdx), this._leftChild(nodeIdx))
+          ? this._rightChild(nodeIdx)
+          : this._leftChild(nodeIdx)
+
+      this._swap(greaterChildIdx, nodeIdx)
+      nodeIdx = greaterChildIdx
+    }
+  }
+}
+
+const networkDelayTime = function (times, n, k) {
+  const distances = new Array(n).fill(Infinity)
+  const adjList = distances.map(() => [])
+
+  for (let i = 0; i < times.length; i++) {
+    const source = times[i][0]
+    const destination = times[i][1]
+    const weight = times[i][2]
+    adjList[source - 1].push({ node: destination - 1, weight })
+  }
+
+  distances[k - 1] = 0
+  const queue = new MyPriorityQueue((a, b) => a < b)
+  queue.push(k - 1)
+
+  while (!queue.isEmpty()) {
+    const popped = queue.pop()
+    const connections = adjList[popped]
+
+    for (let i = 0; i < connections.length; i++) {
+      if (
+        distances[popped] + connections[i].weight <
+        distances[connections[i].node]
+      ) {
+        distances[connections[i].node] =
+          distances[popped] + connections[i].weight
+        queue.push(connections[i].node)
+      }
+    }
+  }
+
+  const max = Math.max(...distances)
+
+  return max === Infinity ? -1 : max
+}
+
 // bellman ford algo
-const networkDelayTime = (times, n, k) => {
+const networkDelayTime2 = (times, n, k) => {
   const distances = new Array(n).fill(Infinity)
   distances[k - 1] = 0
 
