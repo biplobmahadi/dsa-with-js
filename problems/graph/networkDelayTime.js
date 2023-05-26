9b9bd8da0ff9e848b90c049588cc03827a9d8492
biplob <biplobhasan547@gmail.com>
2023-05-26 09:50:43 +0600

    add priority queue with min heap

--- problems/graph/networkDelayTime.js
+++ problems/graph/networkDelayTime.js
@@ -1,3 +1,82 @@
+// create a priority queue with min heap
+class PriorityQueue {
+  constructor(comparator = (a, b) => a < b) {
+    this._heap = []
+    this._comparator = comparator
+  }
+
+  size() {
+    return this._heap.length
+  }
+
+  isEmpty() {
+    return this.size === 0
+  }
+
+  peak() {
+    return this._heap[0]
+  }
+
+  _parent(idx) {
+    return Math.floor((idx - 1) / 2)
+  }
+
+  _leftChild(idx) {
+    return idx * 2 + 1
+  }
+
+  _rightChild(idx) {
+    return idx * 2 + 2
+  }
+
+  _swap(i, j) {
+    const tmp = this._heap[i]
+    this._heap[i] = this._heap[j]
+    this._heap[j] = tmp
+  }
+
+  _compare(i, j) {
+    return this._comparator(this._heap[i], this._heap[j])
+  }
+
+  push(val) {
+    this._heap.push(val)
+
+    let idx = this.size() - 1
+    while (idx > 0 && this._compare(idx, this._parent(idx))) {
+      this._swap(idx, this._parent(idx))
+      idx = this._parent(idx)
+    }
+
+    return this.size()
+  }
+
+  pop() {
+    if (this._heap.size() > 1) {
+      this._swap(0, this.size() - 1)
+    }
+    const popped = this._heap.pop()
+
+    let idx = 0
+    while (
+      (this._leftChild(idx) < this.size() &&
+        this._compare(this._leftChild(idx), idx)) ||
+      (this._rightChild(idx) < this.size() &&
+        this._compare(this._rightChild(idx), idx))
+    ) {
+      const greaterIdx =
+        this._rightChild(idx) < this.size() &&
+        this._compare(this._rightChild(idx), this._leftChild(idx))
+          ? this._rightChild(idx)
+          : this._leftChild(idx)
+      this._swap(greaterIdx, idx)
+      idx = greaterIdx
+    }
+
+    return popped
+  }
+}
+
 const networkDelayTime = (times, n, k) => {}
 
 times = [
@@ -23,3 +102,6 @@ times = [
   [4, 1, 95],
 ]
 console.log(networkDelayTime(times, 5, 1))
+
+const p = new PriorityQueue()
+console.log(p)
