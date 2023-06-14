d5f3ec638d2e470cdcff0fb1d644f2d64e2030f0
biplob <biplobhasan547@gmail.com>
2023-06-14 08:49:42 +0600

    add memorization with caching

--- problems/dp/minCostClimbingStairs.js
+++ problems/dp/minCostClimbingStairs.js
@@ -1,14 +1,20 @@
 const minCostClimbingStairs = (cost) => {
   const n = cost.length
-
+  const minCost = memo()
   return Math.min(minCost(n - 1, cost), minCost(n - 2, cost))
 }
 
-const minCost = (i, cost) => {
-  if (i < 0) return 0
-  if (i === 0 || i === 1) return cost[i]
+const memo = () => {
+  const cache = {}
+  return function minCost(i, cost) {
+    if (i < 0) return 0
+    if (i === 0 || i === 1) return cost[i]
 
-  return cost[i] + Math.min(minCost(i - 1, cost), minCost(i - 2, cost))
+    if (!cache[i]) {
+      cache[i] = cost[i] + Math.min(minCost(i - 1, cost), minCost(i - 2, cost))
+    }
+    return cache[i]
+  }
 }
 
 const cost = [2, 3, 5, 7, 9, 12, 43]
