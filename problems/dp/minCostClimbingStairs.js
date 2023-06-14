85dc7e3ff82d216d303e135714a604c94e8173bf
biplob <biplobhasan547@gmail.com>
2023-06-14 09:39:14 +0600

    update bottom up approach with const space

--- problems/dp/minCostClimbingStairs.js
+++ problems/dp/minCostClimbingStairs.js
@@ -1,19 +1,30 @@
 const minCostClimbingStairs = (cost) => {
   const n = cost.length
-  const dp = []
-  return Math.min(minCost(n - 1, cost, dp), minCost(n - 2, cost, dp))
-}
+  if (n === 0) return 0
+  if (n === 1) return cost[0]
 
-function minCost(i, cost, dp) {
-  if (i < 0) return 0
-  if (i === 0 || i === 1) return cost[i]
+  let dpOne = cost[0]
+  let dpTwo = cost[1]
 
-  if (!dp[i]) {
-    dp[i] =
-      cost[i] + Math.min(minCost(i - 1, cost, dp), minCost(i - 2, cost, dp))
+  for (let i = 2; i < n; i++) {
+    const currentCost = cost[i] + Math.min(dpOne, dpTwo)
+    dpOne = dpTwo
+    dpTwo = currentCost
   }
-  return dp[i]
+
+  return Math.min(dpOne, dpTwo)
 }
+
+// function minCost(i, cost, dp) {
+//   if (i < 0) return 0
+//   if (i === 0 || i === 1) return cost[i]
+
+//   if (!dp[i]) {
+//     dp[i] =
+//       cost[i] + Math.min(minCost(i - 1, cost, dp), minCost(i - 2, cost, dp))
+//   }
+//   return dp[i]
+// }
 
 const cost = [2, 3, 5, 7, 9, 12, 43]
 //2  3  7  10  16  22
