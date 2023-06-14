a270116ee17c343b874a51f233d340959aceb38c
biplob <biplobhasan547@gmail.com>
2023-06-14 09:00:20 +0600

    use array as memorization data structure

--- problems/dp/minCostClimbingStairs.js
+++ problems/dp/minCostClimbingStairs.js
@@ -1,20 +1,18 @@
 const minCostClimbingStairs = (cost) => {
   const n = cost.length
-  const minCost = memo()
-  return Math.min(minCost(n - 1, cost), minCost(n - 2, cost))
+  const dp = []
+  return Math.min(minCost(n - 1, cost, dp), minCost(n - 2, cost, dp))
 }
 
-const memo = () => {
-  const cache = {}
-  return function minCost(i, cost) {
-    if (i < 0) return 0
-    if (i === 0 || i === 1) return cost[i]
+function minCost(i, cost, dp) {
+  if (i < 0) return 0
+  if (i === 0 || i === 1) return cost[i]
 
-    if (!cache[i]) {
-      cache[i] = cost[i] + Math.min(minCost(i - 1, cost), minCost(i - 2, cost))
-    }
-    return cache[i]
+  if (!dp[i]) {
+    dp[i] =
+      cost[i] + Math.min(minCost(i - 1, cost, dp), minCost(i - 2, cost, dp))
   }
+  return dp[i]
 }
 
 const cost = [2, 3, 5, 7, 9, 12, 43]
