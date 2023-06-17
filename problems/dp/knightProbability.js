cc25089155323ef8b1b1ccf73c270caf8492b3aa
biplob <biplobhasan547@gmail.com>
2023-06-18 00:18:58 +0600

    solve knight probability with recursive approach

--- problems/dp/knightProbability.js
+++ problems/dp/knightProbability.js
@@ -9,4 +9,17 @@ const DIRECTIONS = [
   [-1, -2],
 ]
 
-const knightProbability = (n, k, r, c) => {}
+const knightProbability = (n, k, r, c) => {
+  if (r < 0 || r >= n || c < 0 || c >= n) return 0
+  if (k === 0) return 1
+
+  let sum = 0
+  for (let i = 0; i < DIRECTIONS.length; i++) {
+    sum +=
+      knightProbability(n, k - 1, r + DIRECTIONS[i][0], c + DIRECTIONS[i][1]) /
+      8
+  }
+  return sum
+}
+
+console.log(knightProbability(3, 2, 0, 0))
