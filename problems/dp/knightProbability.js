fd80db993c684202f0ea2bcfd8d639aa8130c498
biplob <biplobhasan547@gmail.com>
2023-06-18 12:37:29 +0600

    apply bottom up sol for knight probability problem

--- problems/dp/knightProbability.js
+++ problems/dp/knightProbability.js
@@ -30,4 +30,39 @@ const recurse = (n, k, r, c, dp) => {
   return dp[k][r][c]
 }
 
-console.log(knightProbability(3, 2, 0, 0))
+// console.log(knightProbability(3, 2, 0, 0))
+
+const knightProbabilityBottomUp = (n, k, r, c) => {
+  const dp = new Array(k + 1)
+    .fill(0)
+    .map(() => new Array(n).fill(0).map(() => new Array(n).fill(0)))
+
+  dp[0][r][c] = 1
+
+  for (let step = 1; step <= k; step++) {
+    for (let row = 0; row < n; row++) {
+      for (let col = 0; col < n; col++) {
+        for (let i = 0; i < DIRECTIONS.length; i++) {
+          const dir = DIRECTIONS[i]
+          const prevRow = row + dir[0]
+          const prevCol = col + dir[1]
+
+          if (prevRow >= 0 && prevCol >= 0 && prevRow < n && prevCol < n) {
+            dp[step][row][col] += dp[step - 1][prevRow][prevCol] / 8
+          }
+        }
+      }
+    }
+  }
+
+  let sum = 0
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      sum += dp[k][row][col]
+    }
+  }
+
+  return sum
+}
+
+console.log(knightProbabilityBottomUp(3, 2, 0, 0)) // O(k.n^2) & O(k.n^2)
